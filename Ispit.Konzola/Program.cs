fa5757aae4aec9b0729ce9cbe0d7d8035b4f0e93
Ivan Vidović <ivodkw@gmail.com>
2025-07-08 19:29:08 +0200

    Add project files.

---
+++ Ispit.Konzola/Program.cs
@@ -0,0 +1,74 @@
+﻿using Ispit.Model;
+using Ispit.Model.Klase;
+
+namespace Ispit.Konzola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"***** PARCIJALNI ISPIT LINQ ********");
+            var servisPodataka = new Podaci();
+
+
+            //*********** grupiranje milijunaša ***********
+
+
+            var grupirajPremaBanci = servisPodataka.ListaKlijenata
+                .Where(k => k.Stanje >= 1000000)
+                .GroupBy(k => k.Banka)
+                .Select(m => new GrupiraniMilijunasi()
+                {
+                    Banka = m.Key,
+                    Milijunasi = m.Select(k => $" {k.ImePrezime}")
+                });
+
+
+            foreach (var grupa in grupirajPremaBanci)
+            {
+                Console.Write($"\n{grupa.Banka}:");
+
+                    bool first = true;
+                foreach (var milijunas in grupa.Milijunasi)
+                {
+                    if (first)
+                    {
+                        Console.Write($" {milijunas}");
+                        first = false;
+                    }
+                    else
+                    {
+                        Console.Write($" i {milijunas}");
+                    }
+
+                }
+            }
+
+    
+
+            //************* ispis milijunaša i banke u kojoj se nalazi **************
+
+            var izvjestajMilijunasa = servisPodataka.ListaKlijenata
+                .Where(k => k.Stanje >= 1000000)
+                .Join(servisPodataka.ListaBanki,
+                k => k.Banka,
+                b => b.Simbol,
+                (k, b) => new
+                {
+                    k.ImePrezime, 
+                    Banka = b.Naziv
+                });
+
+
+            Console.WriteLine("\n"); //prazni red 
+            foreach (var item in izvjestajMilijunasa)
+            {
+                Console.WriteLine($"{item.ImePrezime} je u {item.Banka}");
+            }
+
+
+
+
+        }
+    }
+}
---
+++ Ispit.Model/Podaci.cs
@@ -0,0 +1,41 @@
+﻿using Ispit.Model.Klase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ispit.Model
+{
+    public class Podaci
+    {
+        public List<Banka> ListaBanki { get; }
+
+        public List<Klijent> ListaKlijenata { get; }
+
+        public Podaci()
+        {
+            ListaBanki = new List<Banka>()
+            {
+                new Banka() {Naziv = "Zagrebačka Banka", Simbol = "ZABA" },
+                new Banka() {Naziv = "Privredna Banka Zagreb", Simbol = "PBZ" },
+                new Banka() {Naziv = "Međimurska Banka", Simbol = "MB" }
+            };
+
+            ListaKlijenata = new List<Klijent>()
+            {
+                new Klijent() { ImePrezime = "Ivan Vidović", Banka = "ZABA", Stanje = 1987050.98 },
+                new Klijent() { ImePrezime = "Petar Preradović", Banka = "PBZ", Stanje = 9856.57 },
+                new Klijent() { ImePrezime = "Ivana Brlić Mažuranić", Banka = "PBZ", Stanje = 3004987.68 },
+                new Klijent() { ImePrezime = "Nikolina Mikec", Banka = "MB", Stanje = 5875698 },
+                new Klijent() { ImePrezime = "Nitko Nitković", Banka = "ZABA", Stanje = 0.69 },
+                new Klijent() { ImePrezime = "Ivica Todorović", Banka = "PBZ", Stanje = 18987050.98 },
+                new Klijent() { ImePrezime = "Zdravko Mamić", Banka = "MB", Stanje = 1999989.99 },
+                new Klijent() { ImePrezime = "Andrej Plenković", Banka = "ZABA", Stanje = 50.98 },
+                new Klijent() { ImePrezime = "Đelo Hađiselimović", Banka = "MB", Stanje = 8579645.87 },
+                new Klijent() { ImePrezime = "Propali Slučajić", Banka = "ZABA", Stanje = 0}
+            };
+        }
+
+    }
+}
